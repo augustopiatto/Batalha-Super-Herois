f2f8cc50bd220d52859ccaae8bfbf0343a769d34
Augusto <ampiatto@hotmail.com>
2023-11-01 19:53:44 -0300

    Instala nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return <main className=""></main>;
+}
+
+// barra de pesquisar --lugar--
+// checkbox de "batalha" - nessa parte que vou fazer o gerenciamento de estado pra ver quais cartas foram selecionadas
+// Header com avatar do usuário e com a aba de "cartas" como padrão do router
+// Componente de carta sem status
+// Componente de personagem X personagem
+// (Opcional) pro futuro modal para ver o status de cada herói individualmente, e passar pro lado para ver o próximo

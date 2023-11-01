f8b1fee66cf20358453ccf8520c5f4ba3d845d31
Augusto <ampiatto@hotmail.com>
2023-11-01 20:33:35 -0300

    reajusta o app e instala MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-export default function Home() {
-  return <main className=""></main>;
-}
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
-// barra de pesquisar --lugar--
-// checkbox de "batalha" - nessa parte que vou fazer o gerenciamento de estado pra ver quais cartas foram selecionadas
-// Header com avatar do usuário e com a aba de "cartas" como padrão do router
-// Componente de carta sem status
-// Componente de personagem X personagem
-// (Opcional) pro futuro modal para ver o status de cada herói individualmente, e passar pro lado para ver o próximo
+export default function Home() {
+  return (
+    <main className="bg-white">
+      <AppBar position="static">
+        <Toolbar></Toolbar>
+      </AppBar>
+    </main>
+  );
+}

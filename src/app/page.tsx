4a311178b090d78029bc65236e37c49448bb2337
Augusto <ampiatto@hotmail.com>
2023-11-01 21:52:32 -0300

    monta header simples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,49 @@
+"use client";
+
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Avatar from "@mui/material/Avatar";
+import Icon from "@mui/material/Icon";
 
 export default function Home() {
+  function openUserOptions() {
+    return;
+  }
+  function routeToCards() {
+    return;
+  }
+
   return (
-    <main className="bg-white">
+    <main>
       <AppBar position="static">
-        <Toolbar></Toolbar>
+        <Toolbar className="flex relative">
+          <h1 className="absolute left-1/2 uppercase text-2xl">
+            Jornada do Herói
+          </h1>
+          <IconButton
+            className="ml-auto"
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={routeToCards}
+            color="inherit"
+          >
+            <Icon>style</Icon>
+            Cartas
+          </IconButton>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={openUserOptions}
+            color="inherit"
+          >
+            <Avatar />
+          </IconButton>
+        </Toolbar>
       </AppBar>
     </main>
   );

a6be17793b2b7636b8b3cdae23b20aaa6fa7505c
Augusto <ampiatto@hotmail.com>
2023-11-03 19:09:32 -0300

    coloca loading

--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,14 +1,16 @@
 import Card from "./Card";
 import React from "react";
 import { HeroesContext } from "@/contexts/HeroesContext";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Deck() {
   const { filteredDeck } = React.useContext(HeroesContext);
 
   return (
     <div className="pt-10 px-10 flex flex-wrap justify-center gap-10">
+      {(!filteredDeck || !filteredDeck.length) && <CircularProgress />}
       {filteredDeck &&
-        filteredDeck.length &&
+        !!filteredDeck.length &&
         filteredDeck.map((card) => <Card card={card} key={card.id} />)}
     </div>
   );

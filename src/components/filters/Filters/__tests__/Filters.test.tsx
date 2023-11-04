d2f87e2f0979742cdfeeedd517bf9829e97abfc6
Augusto <ampiatto@hotmail.com>
2023-11-04 06:30:03 -0300

    ajeita setup de teste e faz o primeiro teste

--- src/components/filters/Filters/__tests__/Filters.test.tsx
+++ src/components/filters/Filters/__tests__/Filters.test.tsx
@@ -1,13 +1,35 @@
 import { test, expect, describe } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Filters from "../Filters.jsx";
+import { beforeEach } from "node:test";
+import { HeroesContext } from "@/contexts/HeroesContext.jsx";
+import { fakeDeck } from "@/helpers/apimock/fakeDeck.jsx";
+import CardInterface from "@/interfaces/CardInterface.jsx";
+
+function renderFilters(deck: CardInterface[]) {
+  return render(
+    <HeroesContext.Provider value={{ deck }}>
+      <Filters opened={true}></Filters>
+    </HeroesContext.Provider>
+  );
+}
 
 describe("Filters.tsx", () => {
   test("Componente sendo montado", () => {
-    render(<Filters opened={true}></Filters>);
+    renderFilters(fakeDeck);
+    expect(screen.getByText(/Aplicar/i)).toBeDefined();
   });
 
-  test("Filtro sendo aplicado", async () => {});
+  // test("Filtro sendo aplicado", async () => {
+  //   renderFilters(fakeDeck);
+  //   const button = screen.getByText(/Aplicar/i);
+  //   fireEvent.click(button);
+  // });
 
-  test("Filtro sendo removido", async () => {});
+  // test("Filtro sendo removido", async () => {
+  //   renderFilters(fakeDeck);
+  //   const button = screen.getByText(/Aplicar/i);
+  //   fireEvent.click(button);
+  //   fireEvent.click(button);
+  // });
 });

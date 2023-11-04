7585365e506c74b0c04ea58f4d99d687004b7d18
Augusto <ampiatto@hotmail.com>
2023-11-04 08:06:19 -0300

    tira import desnecessario

--- src/components/Deck/__tests__/Deck.test.tsx
+++ src/components/Deck/__tests__/Deck.test.tsx
@@ -1,5 +1,5 @@
 import { test, expect, describe } from "vitest";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { HeroesContext } from "@/contexts/HeroesContext";
 import { BattleContext } from "@/contexts/BattleContext";
 import Deck from "../Deck";
